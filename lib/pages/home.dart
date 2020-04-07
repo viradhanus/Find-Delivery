28f1137c405ec43d35922c76834fa388c14bc63e
viradhanus <e16083@eng.pdn.ac.lk>
2020-04-07 13:48:22 +0530

    modelPHD

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,7 +6,9 @@ import 'package:finddelivery/pages/upload.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 import 'activity_feed.dart';
 
@@ -18,10 +20,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool showSpinner = false;
   bool isAuth = false;
   PageController pageController;
   int pageIndex = 0;
   FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   void initState() {
     super.initState();
@@ -31,13 +35,14 @@ class _HomeState extends State<Home> {
       addFirebaseAuth(account);
       handleSignIn(account);
     }, onError: (err) {
+      showToast('Signing In Failed');
       print('Error signing in: $err');
     });
     // Reauthenticate user when app is opened
     googleSignIn.signInSilently(suppressErrors: false).then((account) {
       handleSignIn(account);
     }).catchError((err) {
-      print('Error signing in: $err');
+      print('silently: $err');
     });
   }
 
@@ -48,19 +53,23 @@ class _HomeState extends State<Home> {
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
-      final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
-      print("signed in " + user.displayName);
+      final FirebaseUser user =
+          (await _auth.signInWithCredential(credential)).user;
+      showToast("Hi, " + user.displayName);
     }
   }
 
   handleSignIn(GoogleSignInAccount account) {
     if (account != null) {
       print('User signed in!: $account');
+
       setState(() {
+        showSpinner = false;
         isAuth = true;
       });
     } else {
       setState(() {
+        showSpinner = false;
         isAuth = false;
       });
     }
@@ -90,6 +99,15 @@ class _HomeState extends State<Home> {
     pageController.jumpToPage(
       pageIndex,
     );
+  }
+
+  void showToast(message) {
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        // toastLength: Toast.LENGTH_LONG,
+        backgroundColor: Colors.blue,
+        textColor: Colors.white);
   }
 
   Widget buildAuthScreen() {
@@ -143,52 +161,59 @@ class _HomeState extends State<Home> {
 
   Widget buildUnAuthScreen() {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Find Delivery',
-              style: TextStyle(
-                fontFamily: 'Signatra',
-                fontSize: 70.0,
-                color: Colors.blueAccent,
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              width: 250.0,
-              height: 250.0,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/delivery_cab.jpg'),
-                    fit: BoxFit.cover),
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            GestureDetector(
-              onTap: login,
-              child: Container(
-                width: 180.0,
-                height: 40.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image:
-                          AssetImage('assets/images/google_signin_button.png'),
-                      fit: BoxFit.cover),
+      body: ModalProgressHUD(
+          inAsyncCall: showSpinner,
+          child: Container(
+            color: Colors.white,
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Find Delivery',
+                  style: TextStyle(
+                    fontFamily: 'Signatra',
+                    fontSize: 70.0,
+                    color: Colors.blueAccent,
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: 250.0,
+                  height: 250.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/delivery_cab.jpg'),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      showSpinner = true;
+                    });
+                    login();
+                  },
+                  child: Container(
+                    width: 180.0,
+                    height: 40.0,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/google_signin_button.png'),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )),
     );
   }
 
