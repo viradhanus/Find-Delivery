41d60ffccd20acc15167c9fb2dee593072c10566
viradhanus <e16083@eng.pdn.ac.lk>
2020-04-09 15:28:22 +0530

    header widget

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+AppBar header(context, {bool isAppTitle = false, String titleText}) {
+  return AppBar(
+    title: Text(
+      isAppTitle ? "Find Delivery" : titleText,
+      style: TextStyle(
+        color: Colors.white,
+        // fontFamily: isAppTitle ? "Signatra" : "",
+        // fontSize: isAppTitle ? 30.0 : 22.0,
+        fontFamily:"Signatra",
+        fontSize: 30.0,
+      ),
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
 }
